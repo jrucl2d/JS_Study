7d17682f36e7577def8e057e668fe008d3c5d680
jrucl2d <ysk789465@gmail.com>
2020-08-20 14:59:22 +0900

    Mine game done

--- es2020/05-Mine/index.js
+++ es2020/05-Mine/index.js
@@ -1,14 +1,25 @@
 const tbody = document.querySelector("#table tbody");
+const hor = +document.querySelector("#hor").value;
+const ver = +document.querySelector("#ver").value;
+const mine = +document.querySelector("#mine").value;
+const result = document.querySelector("#result");
+let overFlag = false;
+
 let dataset = [];
+const code = {
+  normal: 1,
+  opened: 0,
+  mine: -1,
+};
+let opnedCount = 0;
 
 document.querySelector("#exec").addEventListener("click", () => {
-  var hor = +document.querySelector("#hor").value;
-  var ver = +document.querySelector("#ver").value;
-  var mine = +document.querySelector("#mine").value;
-
   // 초기화
   tbody.innerHTML = "";
+  result.textContent = "";
   dataset = [];
+  overFlag = false;
+  opnedCount = 0;
   // 판 만들기
   Array(hor)
     .fill()
@@ -18,8 +29,9 @@ document.querySelector("#exec").addEventListener("click", () => {
       Array(ver)
         .fill()
         .map((v, i) => {
-          arr.push("");
+          arr.push(code.normal);
           let td = document.createElement("td");
+          td.dataset.isMine = "N";
           tr.appendChild(td);
         });
       tbody.appendChild(tr);
@@ -39,26 +51,31 @@ document.querySelector("#exec").addEventListener("click", () => {
   candidate.forEach((v) => {
     let row = Math.floor(v / ver);
     let col = v - row * ver;
-    tbody.rows[row].cells[col].textContent = "X";
-    dataset[row][col] = "X";
+    // tbody.rows[row].cells[col].textContent = "X";
+    tbody.rows[row].cells[col].dataset.isMine = "X";
+    dataset[row][col] = code.mine;
   });
   console.log(candidate);
   console.log(dataset);
+  tbody.addEventListener("click", (e) => leftClick(e));
 });
 
-// tbody.rows[row].cells[col].textContent
-
 // 좌클릭
-tbody.addEventListener("click", (e) => {
+function leftClick(e) {
+  if (overFlag) return;
   if (e.target.nodeName != "TD") return;
-
   let row = e.target.parentNode.rowIndex - 1;
   let col = e.target.cellIndex;
-  tbody.rows[row].cells[col].classList.add("opened");
-  if (dataset[row][col] === "X") {
+  if (dataset[row][col] === code.opened) return;
+
+  // 게임 오버
+  if (dataset[row][col] === code.mine) {
     tbody.rows[row].cells[col].textContent = "펑";
+    result.textContent = "게임 오버 ㅠㅠ";
+    overFlag = true;
     return;
   }
+  // 주변 지뢰 개수 탐색
   let mineList = [dataset[row][col - 1], dataset[row][col + 1]];
   if (row > 0) {
     mineList = mineList.concat([dataset[row - 1][col - 1], dataset[row - 1][col], dataset[row - 1][col + 1]]);
@@ -67,27 +84,53 @@ tbody.addEventListener("click", (e) => {
     mineList = mineList.concat([dataset[row + 1][col - 1], dataset[row + 1][col], dataset[row + 1][col + 1]]);
   }
   let mineCount = mineList.filter((v) => {
-    return v == "X";
+    return v == code.mine;
   }).length;
+
+  // 열음 표시
+  tbody.rows[row].cells[col].classList.add("opened");
+  dataset[row][col] = code.opened;
+  opnedCount += 1;
+
+  // 게임 승리
+  if (opnedCount == ver * hor - mine) {
+    overFlag = true;
+    result.textContent = "승리했습니다!";
+  }
 
   // 0 클릭하면 재귀함수
   if (mineCount === 0) {
+    let mineList = [tbody.rows[row].cells[col - 1], tbody.rows[row].cells[col + 1]];
+    if (row > 0) {
+      mineList = mineList.concat(tbody.rows[row - 1].cells[col - 1], tbody.rows[row - 1].cells[col], tbody.rows[row - 1].cells[col + 1]);
+    }
+    if (row < dataset.length - 1) {
+      mineList = mineList.concat(tbody.rows[row + 1].cells[col - 1], tbody.rows[row + 1].cells[col], tbody.rows[row + 1].cells[col + 1]);
+    }
+    mineList.filter((v) => !!v).map((v) => v.click());
   }
 
-  tbody.rows[row].cells[col].textContent = mineCount;
-});
+  tbody.rows[row].cells[col].textContent = mineCount || "";
+}
 // 우클릭
 tbody.addEventListener("contextmenu", (e) => {
   e.preventDefault();
+  if (overFlag) return;
   if (e.target.nodeName != "TD") return;
   let row = e.target.parentNode.rowIndex - 1;
   let col = e.target.cellIndex;
+  if (dataset[row][col] === code.opened) return;
 
-  if (tbody.rows[row].cells[col].textContent === "X" || tbody.rows[row].cells[col].textContent === "") {
+  if (tbody.rows[row].cells[col].dataset.isMine === "X" || tbody.rows[row].cells[col].dataset.isMine === "N") {
     tbody.rows[row].cells[col].textContent = "!";
+    tbody.rows[row].cells[col].dataset.isMine = "!";
   } else if (tbody.rows[row].cells[col].textContent === "!") {
     tbody.rows[row].cells[col].textContent = "?";
+    tbody.rows[row].cells[col].dataset.isMine = "?";
   } else if (tbody.rows[row].cells[col].textContent === "?") {
-    tbody.rows[row].cells[col].textContent = dataset[row][col];
+    // tbody.rows[row].cells[col].textContent = dataset[row][col] == -1 ? "X" : "";
+    // tbody.rows[row].cells[col].dataset.isMine = dataset[row][col] == -1 ? "X" : "N";
+    tbody.rows[row].cells[col].textContent = "";
+    tbody.rows[row].cells[col].dataset.isMine = "N";
   }
 });
